6be111814c66df6fe964e82da5cdac9e9a6bd7b0
Joe Time <joetime@gmail.com>
2013-07-19 13:05:26 -0400

    now works with ko.observableArrays and normal arrays

--- chart.ko.js
+++ chart.ko.js
@@ -9,21 +9,27 @@ ko.bindingHandlers.pieChart = {
         // Set up any initial state, event handlers, etc. here
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        // 1. Unwrap value, so we can use observables or regular arrays
+        // 2. Create a context, set options, bind chart
+        // 3. Log debug info
 
+        //1
+        var value = valueAccessor();
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+        //2
         if (chart_ko_debug) {
             console.log("chart.ko.js - pieChart - update");
             console.log(element);
-            console.log(valueAccessor());
+            console.log(valueAccessor);
+            console.log(valueUnwrapped);
             console.log(allBindingsAccessor());
             console.log(viewModel);
             console.log(bindingContext);
         }
-        // Create a context, set options, bind chart
-        // 
-        // 
-        var pieCtx = element.getContext("2d");
-        var options = [];
-        new Chart(pieCtx).Pie(valueAccessor(), options);
+        //3
+        var ctx = element.getContext("2d");
+        var options = [];   // would like a way to set these
+        new Chart(ctx).Pie(valueUnwrapped, options);
     }
 };
 
@@ -36,21 +42,27 @@ ko.bindingHandlers.barChart = {
         // Set up any initial state, event handlers, etc. here
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        // 1. Unwrap value, so we can use observables or regular arrays
+        // 2. Create a context, set options, bind chart
+        // 3. Log debug info
 
+        //1
+        var value = valueAccessor();
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+        //2
         if (chart_ko_debug) {
             console.log("chart.ko.js - barChart - update");
             console.log(element);
-            console.log(valueAccessor());
+            console.log(valueAccessor);
+            console.log(valueUnwrapped);
             console.log(allBindingsAccessor());
             console.log(viewModel);
             console.log(bindingContext);
         }
-        // Create a context, set options, bind chart
-        // 
-        // 
-        var barCtx = element.getContext("2d");
-        var options = [];
-        new Chart(barCtx).Bar(valueAccessor(), options);
+        //3
+        var ctx = element.getContext("2d");
+        var options = [];   // would like a way to set these
+        new Chart(ctx).Bar(valueUnwrapped, options);
     }
 };
 
@@ -64,19 +76,26 @@ ko.bindingHandlers.lineChart = {
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
 
+        // 1. Unwrap value, so we can use observables or regular arrays
+        // 2. Create a context, set options, bind chart
+        // 3. Log debug info
+
+        //1
+        var value = valueAccessor();
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+        //2
         if (chart_ko_debug) {
             console.log("chart.ko.js - lineChart - update");
             console.log(element);
-            console.log(valueAccessor());
+            console.log(valueAccessor);
+            console.log(valueUnwrapped);
             console.log(allBindingsAccessor());
             console.log(viewModel);
             console.log(bindingContext);
         }
-        // Create a context, set options, bind chart
-        // 
-        // 
-        var lineCtx = element.getContext("2d");
-        var options = [];
-        new Chart(lineCtx).Line(valueAccessor(), options);
+        //3
+        var ctx = element.getContext("2d");
+        var options = [];   // would like a way to set these
+        new Chart(ctx).Line(valueUnwrapped, options);
     }
 };
