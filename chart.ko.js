ed596618ea9abb6ee9bae4675b7b094a3fa587b7
Joe Time <joetime@gmail.com>
2013-07-19 12:25:51 -0400

    added barChart and lineChart bindings

--- chart.ko.js
+++ chart.ko.js
@@ -3,7 +3,7 @@ var chart_ko_debug = true;
 ko.bindingHandlers.pieChart = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
         if (chart_ko_debug) {
-            console.log("custom binding init");
+            console.log("chart.ko.js - pieChart - init");
         }
         // This will be called when the binding is first applied to an element
         // Set up any initial state, event handlers, etc. here
@@ -11,7 +11,7 @@ ko.bindingHandlers.pieChart = {
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
 
         if (chart_ko_debug) {
-            console.log("custom binding bind");
+            console.log("chart.ko.js - pieChart - update");
             console.log(element);
             console.log(valueAccessor());
             console.log(allBindingsAccessor());
@@ -24,5 +24,59 @@ ko.bindingHandlers.pieChart = {
         var pieCtx = element.getContext("2d");
         var options = [];
         new Chart(pieCtx).Pie(valueAccessor(), options);
+    }
+};
+
+ko.bindingHandlers.barChart = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        if (chart_ko_debug) {
+            console.log("chart.ko.js - barChart - init");
+        }
+        // This will be called when the binding is first applied to an element
+        // Set up any initial state, event handlers, etc. here
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+
+        if (chart_ko_debug) {
+            console.log("chart.ko.js - barChart - update");
+            console.log(element);
+            console.log(valueAccessor());
+            console.log(allBindingsAccessor());
+            console.log(viewModel);
+            console.log(bindingContext);
+        }
+        // Create a context, set options, bind chart
+        // 
+        // 
+        var barCtx = element.getContext("2d");
+        var options = [];
+        new Chart(barCtx).Bar(valueAccessor(), options);
+    }
+};
+
+ko.bindingHandlers.lineChart = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        if (chart_ko_debug) {
+            console.log("chart.ko.js - lineChart - init");
+        }
+        // This will be called when the binding is first applied to an element
+        // Set up any initial state, event handlers, etc. here
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+
+        if (chart_ko_debug) {
+            console.log("chart.ko.js - lineChart - update");
+            console.log(element);
+            console.log(valueAccessor());
+            console.log(allBindingsAccessor());
+            console.log(viewModel);
+            console.log(bindingContext);
+        }
+        // Create a context, set options, bind chart
+        // 
+        // 
+        var lineCtx = element.getContext("2d");
+        var options = [];
+        new Chart(lineCtx).Line(valueAccessor(), options);
     }
 };
